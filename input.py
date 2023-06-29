73f8ddc068dbdea285a99fe523d3a1ec70edfb49
dxmv <dimitirjestepanovic1@gmail.com>
2023-06-29 12:24:52 +0200

    Desktop app completed

--- input.py
+++ input.py
@@ -1,9 +1,38 @@
 import customtkinter
+import psycopg2
+from datetime import datetime, timedelta
+
+DATE_FORMAT="%H:%M"
+
+def get_start_end(time):
+    end_hours, end_minutes = time.split(":")
+    if end_minutes != "00" or end_minutes != "30":
+        if 0 < int(end_minutes) < 30:
+            end_minutes = "00"
+        elif 30 < int(end_minutes) < 60:
+            end_minutes = "30"
+    end = f"{end_hours}:{end_minutes}"
+    return (datetime.strptime(end,DATE_FORMAT) - timedelta(hours=0, minutes=30)).strftime(DATE_FORMAT),end
 
 
 def save_action():
-    text=entry.get()
-    print(text)
+    text = entry.get()
+    current_time = datetime.now().strftime(DATE_FORMAT)
+    start, end = get_start_end(current_time)
+
+    conn = psycopg2.connect(
+        host="localhost",
+        database="TimeTracker",
+        user="postgres",
+        password="dima")
+
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO ACTIVITY (TEXT,START_AT,END_AT,DAY_DATE) VALUES ('{text}','{start}','{end}',CURRENT_DATE)")
+    conn.commit()
+
+    cur.close()
+    conn.close()
+    root.destroy()
 
 
 # Config
@@ -17,15 +46,15 @@ root.geometry("500x350")
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(padx=20, pady=20, fill="both", expand=True)
 # Main Label
-label=customtkinter.CTkLabel(master=frame,text="What have you been doing for the last 30 minutes?",font=("Roboto",18))
-label.pack(pady=12,padx=10)
+label = customtkinter.CTkLabel(master=frame, text="What have you been doing for the last 30 minutes?",
+                               font=("Roboto", 18))
+label.pack(pady=12, padx=10)
 # Entry
-entry = customtkinter.CTkEntry(master=frame,width=300)
+entry = customtkinter.CTkEntry(master=frame, width=300)
 entry.pack(pady=12, padx=10)
 
 # Button
-button=customtkinter.CTkButton(master=frame,text="Submit",hover=True,command=save_action)
-button.pack(padx=10,pady=10)
+button = customtkinter.CTkButton(master=frame, text="Submit", hover=True, command=save_action)
+button.pack(padx=10, pady=10)
 
 root.mainloop()
-
