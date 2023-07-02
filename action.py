25bcb19a663ab4b6558f87c02e39a630b03d67e4
dxmv <dimitirjestepanovic1@gmail.com>
2023-07-02 22:30:55 +0200

    Table for today

--- action.py
+++ action.py
@@ -1,10 +1,15 @@
 class Action:
-    def __init__(self, start, end, text):
+    def __init__(self, start, end, text,date):
         self.start = start
         self.end = end
         self.text = text
+        self.date=date
 
-    def __init__(self, start, end):
+    def __init__(self, start, end,date):
         self.start = start
         self.end = end
         self.text = None
+        self.date = date
+
+    def set_text(self,text):
+        self.text=text
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # Flask app for showing the database contents.
+import datetime
+
 import psycopg2
 from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
+from action import Action
 
 load_dotenv()
 app = Flask(__name__)
@@ -27,6 +30,12 @@ def home():
 
 @app.route("/today")
 def today():
+    day = {}
+    for hour in range(24):
+        start_one, start_two = [f"{convert_hours_string(hour)}:00", f"{convert_hours_string(hour)}:30"]
+        end_one, end_two = [f"{convert_hours_string(hour)}:30", f"{convert_hours_string(hour+1)}:00"]
+        day[f"{start_one}-{end_one}"] = Action(start_one, end_one,datetime.date)
+        day[f"{start_two}-{end_two}"] = Action(start_two, end_two,datetime.date)
     cur = conn.cursor()
 
     # Execute a select query
@@ -38,13 +47,18 @@ def today():
     # Process the rows
     for row in rows:
         [text,start,end,date]=[row[1],row[2].strftime(TIME_FORMAT),row[3].strftime(TIME_FORMAT),row[4]]
-        print(text,start,end,date)
+        key=f"{start}-{end}"
+        day[key]=Action(start,end,text,date)
 
     # Close the cursor and connection
     cur.close()
     conn.close()
-    return render_template("today.html")
+    return render_template("today.html",day=day)
 
+def convert_hours_string(hour):
+    if hour<10:
+        return f"0{hour}"
+    return f"{hour}"
 
 # running application
 if __name__ == '__main__':
